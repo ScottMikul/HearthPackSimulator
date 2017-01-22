dc3c6814966501abd33ebd74c79e0ad8497a9ef5
ScottMikul <Scott.p.mikul@gmail.com>
2017-01-21 21:29:09 -0500

    editing th ekey
    use your own key please

--- getcards.js
+++ getcards.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var options = {
   url: 'https://omgvamp-hearthstone-v1.p.mashape.com/cards?collectible=1',
   headers: {
-    'X-Mashape-Key': '3sXhHLo4PnmshOetV8wh5iYWtFrap1IUEQxjsnaDFS4hcJ81ZJ'
+    'X-Mashape-Key': 'ENTER YOUR KEY HERE'
   }
 };
 
